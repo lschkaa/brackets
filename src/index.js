956b1164bfaf8d3719560a59d5c6bfc75c1c403c
Fadeeva Olesia Evgenevna <lschkaaaa@gmail.com>
2024-10-28 00:52:30 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+  const bracketsMap = new Map(bracketsConfig);
+  const openBrackets = new Set(bracketsConfig.map(pair => pair[0]));
+  const closeBrackets = new Set(bracketsConfig.map(pair => pair[1]));
+
+  for (const char of str) {
+    if (openBrackets.has(char)) {
+      if (closeBrackets.has(char) && stack[stack.length - 1] === char) {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    } else if (closeBrackets.has(char)) {
+      if (stack.length === 0 || bracketsMap.get(stack.pop()) !== char) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
